c16ce23f1a8216a80c44a9e284702463336a3c46
RonitYon2112 <ronityon2112@outlook.com>
2025-03-30 17:26:39 +0800

    Initial commit

---
+++ src/main/java/com/ronityon2112/betterwoodendecorationmod/BetterWoodenDecorationModClient.java
@@ -0,0 +1,10 @@
+package com.ronityon2112.betterwoodendecorationmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterWoodenDecorationModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/ronityon2112/betterwoodendecorationmod/BetterWoodenDecorationModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ronityon2112.betterwoodendecorationmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterWoodenDecorationModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
